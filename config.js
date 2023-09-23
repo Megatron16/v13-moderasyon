a5ace697e6d08bad0bb5d2b405bb3f5078e6cc3e
Megatron16 <143656283+Megatron16@users.noreply.github.com>
2023-09-24 02:43:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,44 +1,44 @@
 module.exports = {
     embedColor: "RANDOM",
-    admins: ["Bot kurucusu ID"],
-    serverId: "Sunucu ID",
-    token: "Bot Tokeni",
-    mongoURL: "Mongo URL'si",
+    admins: ["1038366912133988483"],
+    serverId: "1146491713188265994",
+    token: "MTE0OTQ5NzUxNDM4NzU4NzA4Mg.GU2Lxh.hPO_kUuoM-svcmDf9o5vTES5Tdz5RIbHFLPsHo",
+    mongoURL: "mongodb+srv://turkbeyli225:<password>@cluster0.cydilbr.mongodb.net/",
     status: ["Serendia ❤️ Astpod", "Astpod ❤️ Serendia"],
 
-    muteLimit: 10,
-    banLimit: 5,
-    vmuteLimit: 10,
-    jailLimit: 6,
+    muteLimit: 0,
+    banLimit: 0,
+    vmuteLimit: 0,
+    jailLimit: 0,
 
     tag: "Tagınız",
     tagsıztag: "•",
 
-    booster: "Booster rolünüz",
-    unregister: "Unregister rolünüz",
-    erkek: "Erkek rolünüz",
-    kız: "Kız rolünüz",
+    booster: "1146491713364439227",
+    unregister: "1146525949404393483",
+    erkek: "1146491713397981439", "1146491713397981441"
+    kız: "1146491713397981438", "1146491713397981440"
 
-    muted: "Chat mute rolü",
-    cezalı: "Cezalı rolü",
-    şüpheli: "Şüpheli rolü",
+    muted: "1146491713339269172",
+    cezalı: "1146491713339269170",
+    şüpheli: "1146491713473482765",
 
-    banHammer: ["Ban rolü", "2.rolü ekleyebilirsiniz"],
-    registerHammer: ["Register rolü", "2.rolü ekleyebilirsiniz"],
-    muteHammer: ["Mute rolü", "2.rolü ekleyebilirsiniz"],
-    voiceMuteHammer: ["Voice Mute rolü", "2.rolü ekleyebilirsiniz"],
-    jailHammer: ["Jail rolü", "2.rolü ekleyebilirsiniz"],
-    transportHammer: ["Transport rolü", "2.rolü ekleyebilirsiniz"],
+    banHammer: [1146498083719491595", "2.rolü ekleyebilirsiniz"],
+    registerHammer: ["1146499265095553235", "2.rolü ekleyebilirsiniz"],
+    muteHammer: [1146498671807045713", "2.rolü ekleyebilirsiniz"],
+    voiceMuteHammer: ["1146491713339269173", "2.rolü ekleyebilirsiniz"],
+    jailHammer: ["1146498502399098940", "2.rolü ekleyebilirsiniz"],
+    transportHammer: ["T1149454076862550057", "2.rolü ekleyebilirsiniz"],
 
-    banLog: "Ban log kanal ID",
-    muteLog: "Mute log kanal ID",
-    vmuteLog: "Voice Mute log kanal ID",
-    jailLog: "Jail log kanal ID",
-    botkomut: "bot komut kanal ID",
+    banLog: 1146491720364720258",
+    muteLog: "1146519898021957722",
+    vmuteLog: "1146491720364720266",
+    jailLog: "1146491720142426190",
+    botkomut: "1146491715113455629",
 
 
-    ok: "Evet emoji ID",
-    no: "Hayır emoji ID",
-    okname: "Evet emoji ismi",
-    noname: "Hayır emoji ismi"
+    ok: "1155288073656094841",
+    no: "1155287835486720122",
+    okname: "evet",
+    noname: "hayır"
 }
